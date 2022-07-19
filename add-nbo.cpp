8b06ec04f32879b47d5e276526f3cb5b59754c65
mj <meanjunghong@naver.com>
2022-07-19 23:00:27 +0900

    uploaded

---
+++ add-nbo.cpp
@@ -0,0 +1,25 @@
+#include<stdint.h>
+#include<stdio.h>
+#include<netinet/in.h>
+
+int main(){
+	FILE *fp1 = fopen("thousand.bin","r");
+	char buffer1[5]={0, };
+	
+	FILE *fp2 = fopen("five-hundred.bin","r");
+	char buffer2[5] = {0, };
+
+	if(fp1 && fp2){
+		fread(buffer1, 5, 1, fp1);
+		fclose(fp1);
+
+		fread(buffer2, 5, 1, fp2);
+		fclose(fp2);
+
+		uint32_t value1 = ntohl(*(uint32_t*)buffer1);
+		uint32_t value2 = ntohl(*(uint32_t*)buffer2);
+		uint32_t value3 = value1 + value2;
+		printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", value1, value1, value2, value2, value3, value3);
+	}
+	return 0;
+}
